98c9cbd40189eb8ed7089e8589a61416a370de14
Mathias Henriksen <math.henriksen@gmail.com>
2023-12-01 09:57:18 +0100

    day 1 solution

--- src/days/day1.rs
+++ src/days/day1.rs
@@ -1,4 +1,50 @@
+use itertools::{self, Itertools};
+
+fn contains_digit10(a: &str) -> Option<char> {
+    if let Some(d) = a.chars().find(|c| c.is_digit(10)) {
+        Some(d)
+    } else {
+        match a {
+            _ if a.contains("zero") => Some('0'),
+            _ if a.contains("one") => Some('1'),
+            _ if a.contains("two") => Some('2'),
+            _ if a.contains("three") => Some('3'),
+            _ if a.contains("four") => Some('4'),
+            _ if a.contains("five") => Some('5'),
+            _ if a.contains("six") => Some('6'),
+            _ if a.contains("seven") => Some('7'),
+            _ if a.contains("eight") => Some('8'),
+            _ if a.contains("nine") => Some('9'),
+            _ => None,
+        }
+    }
+}
+
+fn solve_2(input: &str) -> usize {
+    input
+        .lines()
+        .flat_map(|l| {
+            let first = (0..l.len())
+                .find_map(|s| contains_digit10(&l[..=s]))
+                .unwrap();
+            let last = (0..l.len())
+                .find_map(|s| contains_digit10(&l[(l.len() - (s + 1))..]))
+                .unwrap();
+            let s = format!("{first}{last}");
+            s.parse::<usize>()
+        })
+        .sum()
+}
+
+fn solve_1(input: &str) -> usize {
+    input
+        .lines() // could also be \n\n
+        .map(|l| l.chars().filter(|c| c.is_digit(10)).collect_vec())
+        .map(|v| format!("{}{}", v.first().unwrap(), v.last().unwrap()))
+        .flat_map(|s| s.parse::<usize>())
+        .sum()
+}
+
 pub fn solve(input: &str) -> (usize, usize) {
-    println!("{input}");
-    (0,0)
+    (solve_1(input), solve_2(input))
 }
